e68224d2861d13b77ddf081c9199ac29ae22f230
B-jisoo <mrk334kr@naver.com>
2022-06-13 04:54:17 +0900

    백준 2, 3단계

--- 알고리즘 기초1/9498번 시험성적.py
+++ 알고리즘 기초1/9498번 시험성적.py
@@ -18,11 +18,11 @@ num = int(input())
 
 if 100 >=num >90:
   print("A")
-elif 89 >num >80:
+elif 89 >=num >=80:
   print("B")
-elif 79 >num >70:
+elif 79 >=num >=70:
   print("C")
-elif 69 >num >60:
+elif 69 >=num >=60:
   print("D")
 else:
   print("F")
